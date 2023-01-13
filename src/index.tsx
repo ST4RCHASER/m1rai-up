b68c26cd234a21d230708a55eda9eb9498fb63ab
Gusb3ll <Kitpipat.0647@gmail.com>
2023-01-14 03:29:32 +0700

    feat: use windicss & minify html

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
+import 'windi.css';
+import './css/global.css';
+import './css/animate.css';
 import { render } from 'solid-js/web';
 
 import { Upload } from './components/upload.component';
-import "./css/global.css";
-import "./css/animate.css";
 render(() => <Upload />, document.getElementById('root') as HTMLElement);
